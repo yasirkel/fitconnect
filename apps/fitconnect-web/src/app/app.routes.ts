d84284a40a47a37c5013ca90ee9e7fb4d4f940e1
Yasir <heymanmynameiskorg12@gmail.com>
2025-12-13 17:01:10 +0100

    Edit & Delete functionaliteit toegevoegd met routing

--- apps/fitconnect-web/src/app/app.routes.ts
+++ apps/fitconnect-web/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Route } from '@angular/router';
 import { ClubsComponent } from './components/clubs/clubs.component';
 import { ClubDetailComponent } from './components/club-detail/club-detail.component';
 import { CreateClubComponent } from './components/create-club/create-club.component';
+import { EditClubComponent } from './components/edit-club/edit-club.component';
 
 export const appRoutes: Route[] = [
   { path: '', redirectTo: 'clubs', pathMatch: 'full' },
 
   // Altijd specifiece routes eerst neerzetten
   { path: 'clubs/create', component: CreateClubComponent },
+  { path: 'clubs/:id/edit', component: EditClubComponent },
   { path: 'clubs/:id', component: ClubDetailComponent },
   { path: 'clubs', component: ClubsComponent },
 
--- apps/fitconnect-web/src/app/components/club-detail/club-detail.component.ts
+++ apps/fitconnect-web/src/app/components/club-detail/club-detail.component.ts
@@ -2,22 +2,26 @@ import { Component, OnInit, inject } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ClubsService, Club } from '../../services/clubs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-club-detail',
   standalone: true,
   imports: [NgIf, NgFor, RouterLink],
   templateUrl: './club-detail.component.html',
-  styleUrl: './club-detail.component.css',
+  styleUrls: ['./club-detail.component.css'],
 })
 export class ClubDetailComponent implements OnInit {
   // services via dependency injection
   private route = inject(ActivatedRoute);
   private clubsService = inject(ClubsService);
+  private router = inject(Router);
 
   club: Club | null = null;
   loading = true;
   error: string | null = null;
+  deleting = false;
 
   ngOnInit(): void {
     // Lees het :id deel uit de URL
@@ -41,4 +45,30 @@ export class ClubDetailComponent implements OnInit {
       },
     });
   }
+
+  onDelete(): void {
+  if (!this.club) return;
+
+  const confirmed = window.confirm(
+    `Are you sure you want to delete "${this.club.name}"?`
+  );
+  
+  if (!confirmed) return;
+
+  this.deleting = true;
+  this.error = null;
+
+  this.clubsService.deleteClub(this.club.id).subscribe({
+    next: () => {
+      this.deleting = false;
+      this.router.navigate(['/clubs']);
+    },
+    error: (err) => {
+      console.error('Error deleting club', err);
+      this.error = 'Failed to delete club';
+      this.deleting = false;
+    },
+  });
+}
+
 }
