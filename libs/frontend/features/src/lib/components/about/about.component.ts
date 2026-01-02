46b50393a5c798a2d12a2c0d74c55c3327b00899
Yasir <heymanmynameiskorg12@gmail.com>
2026-01-02 11:19:33 +0100

    About page frontend & backend toegevoegd en inhoudelijk minimaal gevuld

---
+++ libs/frontend/features/src/lib/components/about/about.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fitconnect-about',
+  standalone: true,
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css'],
+})
+export class AboutComponent {}
